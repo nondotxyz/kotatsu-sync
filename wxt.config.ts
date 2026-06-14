6ca707c2be8e9e8c306b611a3ebe735825a07408
nondotxyz <shippudennaruto668@gmail.com>
2026-06-14 09:00:56 +0700

    add safari to target browsers to build

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,21 +3,21 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-    modules: ["@wxt-dev/module-vue"],
-    imports: false,
-    targetBrowsers: ["firefox", "chrome"],
-    srcDir: "src",
-    vite: ({ mode }) => ({
-        plugins: [
-            tailwindcss({
-                optimize: true,
-            }),
-        ],
-        build: {
-            sourcemap: true,
-        },
-    }),
-    webExt: {
-        disabled: true,
-    },
+	modules: ["@wxt-dev/module-vue"],
+	imports: false,
+	targetBrowsers: ["firefox", "chrome", "safari"],
+	srcDir: "src",
+	vite: ({ mode }) => ({
+		plugins: [
+			tailwindcss({
+				optimize: true,
+			}),
+		],
+		build: {
+			sourcemap: true,
+		},
+	}),
+	webExt: {
+		disabled: true,
+	},
 });
